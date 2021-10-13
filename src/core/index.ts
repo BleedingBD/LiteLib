ef531790eb5821dcece09f386d98ba3922476308
Qb <qb@qbit.moe>
2021-10-13 02:31:52 +0200

    Expose static API in Core module

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,2 +1,6 @@
 export {default as Plugin} from "./Plugin";
 export {default as Updater} from "./Updater";
+export {default as Modules} from "../common/Modules";
+export {default as Logger} from "../common/Logger";
+export {default as Notices} from "../common/Notices";
+export {default as Toasts} from "../common/Toasts";
